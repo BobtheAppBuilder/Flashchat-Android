d8ea578e6a899f7a52be2f65bde3cffb49f41bb5
DaveTheBarbarian <dwstanley@live.com>
2017-12-04 18:59:47 -0500

    update

---
+++ app/src/main/java/com/StanleyDavid/flashchatnewfirebase/ChatListAdapter.java
@@ -0,0 +1,10 @@
+package com.StanleyDavid.flashchatnewfirebase;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by StevenSeagul on 11/30/2017.
+ */
+
+public class ChatListAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/StanleyDavid/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,8 @@
+package com.StanleyDavid.flashchatnewfirebase;
+
+/**
+ * Created by StevenSeagul on 11/30/2017.
+ */
+
+public class InstantMessage {
+}
